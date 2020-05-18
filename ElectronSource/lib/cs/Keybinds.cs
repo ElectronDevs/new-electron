9beb8c1cd03b072868fdca1fbefe8951d2c4d05f
LogisticEntertainment <53105767+LogisticEntertainment@users.noreply.github.com>
2020-05-17 22:04:53 -0700

    started to organize

---
+++ ElectronSource/lib/cs/Keybinds.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Electron.lib.cs
+{
+    class Keybinds
+    {
+
+    }
+    public static class DefaultHotKeyProfiles
+    {
+        public static Key AirJump = Key.L;
+        public static Key highjump = Key.NumPad4;
+        public static Key reach = Key.NumPad2;
+        public static Key Speed = Key.NumPad8;
+        public static Key fullbright = Key.N;
+        public static Key aimbot = Key.NumPad8;
+        public static Key Rapidhit = Key.NumPad5;
+        public static Key Nofall = Key.NumPad1;
+        public static Key noknockback = Key.NumPad3;
+        public static Key ShowCoordinates = Key.NumPad7;
+        public static Key FakeCreative = Key.NumPad4;
+        public static Key Glide = Key.F;
+        public static Key bhop = Key.R;
+        public static Key step = Key.M;
+        public static Key killaura = Key.O;
+        public static Key instabreak = Key.J;
+        public static Key phase = Key.P;
+    }
+}
